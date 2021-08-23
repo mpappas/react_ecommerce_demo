59604dbb71f901e97cd2a4beb62318f01441d09e
Manos Pappas <mano.pappa@gmail.com>
2021-08-23 23:25:13 +0100

    Update app with shop page, collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
